0392376908489cb2848277e0eafaea2d7a58c296
Phillipe <phillipefs@msn.com>
2024-04-09 08:39:38 -0300

    Creating simple bucket

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "google_storage_bucket" "gcs_bucket" {
+    name = "test-bucket-random-01893"
+    location = "us-central1"
+  
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "datapipelines-419810"
+region     = "us-central1"
