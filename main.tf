1857d0e857579225e825fbdd30ca269480685d08
Phillipe <phillipefs@msn.com>
2024-04-10 20:47:08 -0300

    Test Pipeline CI/CD terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
-resource "google_storage_bucket" "gcs_bucket" {
-    name = "test-bucket-random-01893"
-
+resource "google_storage_bucket" "my_bucket" {
+  name          = "my-unique-bucket-name"
+  location      = "US"
+  force_destroy = true
 }
